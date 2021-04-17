40e280a08532fca6cf5c81a0152a556ca75a2544
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-04-17 07:58:35 +0300

    Update and rename Game.lua to Game.emojic

--- Game.lua
+++
@@ -1,7 +0,0 @@
-if (x == 0)  then print("movement slow") end --> this will print "true"
-if (x == 1) then print("slow democracy") end
-if (x == 2) then print("walk slow and drink much water") end
-if (x == 3) then print("My understanding is the sum of our misunderstandings") end
-if (x == 4) then print("we were fudging just to stay awake") end
-if (x == 5) then print("craziness functions like chaos so we should better let chaos function") end
-if (x == 6) then print("You are diffending pig ethics. There is no other word") end
