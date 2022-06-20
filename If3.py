44dc25797318bd2b32e80bbbe6d5f4bf0f45dc61
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-06-20 10:45:52 +0000

    Update and rename Truth.py to If3.py

---
+++ If3.py
@@ -0,0 +1,4 @@
+z = 2112
+y = 2119
+if y > z:
+    print("cool")
--- Truth.py
+++
@@ -1,5 +0,0 @@
-x = "Human develpment can continue even after 18 years old! But it's risky + truth = lies sometimes."
-x2 = "Human develpment can continue even after 18 years old! But it's risky + truth = lies sometimes."
-
-print("Truth = lies")
-print(bool(x2))
