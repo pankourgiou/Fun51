e513fedd3910a7ba74e127b9d818be5c9312048b
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-12-31 03:28:57 +0200

    Update Translate_Bloom.py

--- Translate_Bloom.py
+++ Translate_Bloom.py
@@ -1,4 +1,31 @@
 #Import translators module
+#The alphabet sound from A-Z are:
+#kah
+#dah
+#lah
+#vah
+#zah
+#nah
+#mah
+#rah
+#fah
+#sah
+#tah
+#pah
+#yah
+#gah
+#wah
+#jah
+#bah
+#xah
+#qah
+#cah
+#zah
+#lah
+#mah
+#fah
+#rah
+#sah
 import translators as ts
 
 text = 'cheese'
